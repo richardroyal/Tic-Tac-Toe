aa41e78d5cf25fc994ac012d86f7eecdb3376c1d
Richard <richard@saidigital.co>
2014-04-14 00:52:30 -0400

    Worked on added database for tracking wins.

--- web.js
+++ web.js
@@ -2,6 +2,13 @@ var express = require("express");
 var logfmt = require("logfmt");
 var sass = require("node-sass");
 var app = express();
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(":memory:");
+
+db.serialize(function() {
+  db.run("CREATE TABLE results (wins integer, losses integer)");
+});
+
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -20,6 +27,11 @@ app.get('/', function(req, res) {
   });  
 });
 
+app.get('/results/win', function(req, res){
+  db.query("SELECT wins FROM results LIMIT 1", function(records){
+    res.write( records );
+  });
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
